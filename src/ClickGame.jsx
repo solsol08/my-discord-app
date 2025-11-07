dfa123650e4a70f0ba538fb7c0a57707d8ea6c35
NouJouJin <108597930+NouJouJin@users.noreply.github.com>
2025-11-08 08:36:55 +0900

    Revert "fix: バグ修正とコード品質向上"

--- src/ClickGame.jsx
+++ src/ClickGame.jsx
@@ -1,24 +1,15 @@
-/**
- * クリックゲームコンポーネント
- * itch.io からクリックゲームを埋め込み表示
- */
 export default function ClickGame() {
   return (
-    <div style={{ padding: "20px", textAlign: "center" }}>
-      <h2>🐄 クリックゲーム</h2>
-      <iframe
-        frameBorder="0"
-        src="https://itch.io/embed/3613266"
-        width="552"
-        height="167"
-        style={{ margin: "0 auto" }}
-        title="クリックゲーム by solsol08"
-      >
-        <a href="https://solsol08.itch.io/cattle-click-game">
-          クリックゲーム２ by solsol08
-        </a>
-      </iframe>
-    </div>
+    <iframe
+      frameBorder="0"
+      src="https://itch.io/embed/3613266"
+      width="552"
+      height="167"
+    >
+      <a href="https://solsol08.itch.io/cattle-click-game">
+        クリックゲーム２ by solsol08
+      </a>
+    </iframe>
   );
 }
 
